bd2b8de9e653cbb6f561b53b8eece1bdae07f8f2
Kiisi <kiisifelix06@gmail.com>
2023-09-19 08:05:41 +0100

    Initialize flutter

---
+++ lib/presentation/resources/routes_manager.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import '../splash/splash.dart';
+import '../login/login.dart';
+import '../onboarding/onboarding.dart';
+import '../register/register.dart';
+import '../forgot_password/forgot_password.dart';
+import '../store_details/store_details.dart';
+import './strings_manager.dart';
+
+class Routes {
+  static const String splashRoute = "/";
+  static const String onBoardingRoute = "/onboarding";
+  static const String loginRoute = "/login";
+  static const String registerRoute = "/register";
+  static const String forgetPasswordRoute = "/forget-password";
+  static const String mainRoute = "/main";
+  static const String storeDetailsRoute = "/store-details";
+}
+
+class RouteGenerator {
+  static Route<dynamic> getRoute(RouteSettings routeSettings) {
+    switch (routeSettings.name) {
+      case Routes.splashRoute:
+        return MaterialPageRoute(builder: (_) => const SplashView());
+      case Routes.loginRoute:
+        return MaterialPageRoute(builder: (_) => const LoginView());
+      case Routes.onBoardingRoute:
+        return MaterialPageRoute(builder: (_) => const OnBoardingView());
+      case Routes.registerRoute:
+        return MaterialPageRoute(builder: (_) => const RegisterView());
+      case Routes.forgetPasswordRoute:
+        return MaterialPageRoute(builder: (_) => const ForgotPasswordView());
+      case Routes.storeDetailsRoute:
+        return MaterialPageRoute(builder: (_) => const StoreDetailsView());
+      default:
+        return unDefinedRoute();
+    }
+  }
+
+  static Route<dynamic> unDefinedRoute() {
+    return MaterialPageRoute(
+      builder: (_) => Scaffold(
+        appBar: AppBar(
+          title: const Text(AppString.noRouteFound),
+        ),
+        body: const Center(
+          child: Text(AppString.noRouteFound),
+        ),
+      ),
+    );
+  }
+}
